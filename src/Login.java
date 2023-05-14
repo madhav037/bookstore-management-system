ac3cc79ec08fd14f5aa2b45b682e7bfd20c65175
madhav <109799001+madhav037@users.noreply.github.com>
2023-05-14 17:48:33 +0530

    initial

---
+++ src/Login.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+
+public class Login extends JFrame {
+
+    Login() {
+        setSize(1200,1200);
+        setVisible(true);
+
+    }
+    public static void main(String[] args) {
+        new Login();
+
+    }
+}
