11d3dd7ba7e626ca7c518c3637a358b13be6d3f6
madhav <109799001+madhav037@users.noreply.github.com>
2023-05-14 17:53:34 +0530

    done exit on close

--- src/Login.java
+++ src/Login.java
@@ -5,7 +5,7 @@ public class Login extends JFrame {
     Login() {
         setSize(1200,1200);
         setVisible(true);
-
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
     }
     public static void main(String[] args) {
         new Login();
